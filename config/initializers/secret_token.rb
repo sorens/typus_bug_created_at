060bcb08a4da9b6d90ee791c21259246cfc97f84
Steve Orens <steve@orens.com>
2011-05-04 09:21:07 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Typus1::Application.config.secret_token = '6cb546c3fbb0b3bb7fd1bfe9badcd7338964d3c5b397b191673f8aa63c7ab9f922a22655b897406a2e68b93f4d1440db5ddd27cfe66db605bbcafedee97fe47d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Typus1::Application.config.session_store :cookie_store, :key => '_typus1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Typus1::Application.config.session_store :active_record_store
